7d124897d14d81adcfd86895c772bd983a9b5786
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-18 16:28:47 +0530

    Update google.py

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -1,14 +1,18 @@
 """ Powered by @Google
 Available Commands:
-.google search <query>
+.go <query>
 .google image <query>
 .google reverse search"""
 
 import asyncio
 import os
+from re from re import findall
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from requests import get
+from urllib.parse import quote_plus
+from urllib.error import HTTPError
 from google_images_download import google_images_download
 from gsearch.googlesearch import search
 from uniborg.util import admin_cmd
@@ -18,22 +22,26 @@ def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
 
 
-@borg.on(admin_cmd("google search (.*)"))
+@borg.on(admin_cmd("go (.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    start = datetime.now()
-    await event.edit("Processing ...")
-    input_str = event.pattern_match.group(1) # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
-    search_results = search(input_str, num_results=Config.GOOGLE_SEARCH_COUNT_LIMIT)
-    output_str = " "
-    for text, url in search_results:
-        output_str += " 👉🏻  [{}]({}) \n\n".format(text, url)
-    end = datetime.now()
-    ms = (end - start).seconds
-    await event.edit("searched Google for {} in {} seconds. \n{}".format(input_str, ms, output_str), link_preview=False)
-    await asyncio.sleep(5)
-    await event.edit("Google: {}\n{}".format(input_str, output_str), link_preview=False)
+    await event.edit("`UniBorg is Getting Information From Google Please Wait... ✍️🙇`")
+        match_ = event.pattern_match.group(1)
+        match = quote_plus(match_)
+        if not match:
+            await event.edit("`I can't search nothing !!`")
+            return
+        plain_txt = get(f"https://www.startpage.com/do/search?cmd=process_search&query={match}", 'html').text
+        soup = BeautifulSoup(plain_txt, "lxml")
+        msg = ""
+        for result in soup.find_all('a', {'class': 'w-gl__result-title'}):
+            title = result.text
+            link = result.get('href')
+            msg += f"**{title}**{link}\n"
+        await event.edit(
+            "**Google Search Query:**\n\n`" + match_ + "`\n\n**Results:**\n" + msg,
+            link_preview = False
 
 
 @borg.on(admin_cmd("google image (.*)"))
