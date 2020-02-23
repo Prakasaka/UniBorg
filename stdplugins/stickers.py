8f44840388bc799667014fabfab2c7a4fd08a02b
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-02-23 15:18:11 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -47,7 +47,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = rf"{userid}'s Pack"
-    packshortname = f"{userid}"  # format: Uni_Borg_userid
+    packshortname = f"Uni{userid}"  # format: Uni_Borg_userid
 
     await event.edit("Grabbing Sketch Brush & Painting this Picture...")
 
